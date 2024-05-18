3c22816797c2d5800e3a1e318740596cdbd2f052
Duncan Ochieng <65547730+DunteCityzen@users.noreply.github.com>
2024-05-18 08:58:59 +0300

    Solve errors in startapihacking.sh
    Earlier there were mistakes in the script and I didn't realize until later on. Worry no more though coz I've solved the problems with the script and is now guaranteed to get your started rapidly with APIhacking!!!

--- startapihacking.sh
+++ startapihacking.sh
@@ -6,6 +6,7 @@
 ## Installing dependencies(Just to be sure)
 echo 'Installing dependencies'
 sudo apt install pip git golang-go docker-compose docker.io curl
+cd ~/
 
 ## Installing tools for API hacking
 echo "Installing API hacking tools"
@@ -15,18 +16,21 @@ mkdir Opt && cd Opt
 echo "Installing jwt_tool"
 git clone https://github.com/ticarpi/jwt_tool.git && cd jwt_tool
 python -m pip install termcolor cprint pycryptodomex requests
-sudo chmod +x jwt_tool && sudo ln -s ./jwt_tool /usr/local/bin/jwt_tool && cd ..
+sudo chmod +x jwt_tool && sudo ln ./jwt_tool /usr/local/bin/jwt_tool
+cd ~/Opt
 
 ### Installing kiterunner
 echo "Installing kiterunner" 
 git clone https://github.com/assetnote/kiterunner.git && cd kiterunner
 sudo make build
-sudo ln -s ~/dist/kr /usr/local/bin/kr
+sudo ln ./dist/kr /usr/local/bin/kr
+cd ~/Opt
 
 ### Installing Arjun
 echo "Installing Arjun"
 git clone https://github.com/s0md3v/Arjun.git && cd Arjun
-python setup.py && cd ..
+python setup.py install
+cd ~/Opt
 
 ### Installing zaproxy
 echo 'Installing zaproxy ...'
@@ -36,9 +40,8 @@ echo 'Remember to enable OpenAI support'
 ### Installing Postman
 echo "installing Postman"
 curl -o postman.tar.gz https://dl.pstmn.io/download/latest/linux_64
-mkdir Postman
-tar -xzvf postman.tar.gz -C ./Postman && sudo rm postman.tar.gz
-sudo ln -s ./Postman/app/postman /usr/local/bin/postman
+tar -xvf postman.tar.gz && sudo rm postman.tar.gz
+sudo ln ./Postman/app/postman /usr/local/bin/postman
 
 ### Installing mitmproxy2swagger
 echo "Installing mitmproxy2swagger"
@@ -46,23 +49,22 @@ sudo pip install mitmproxy2swagger
 
 ## Setting Up the labs
 echo "Setting up the labs"
-cd .. && mkdir APIhacking && cd APIhacking
-mkdir crAPI && mkdir vAPI
+cd ~/
+mkdir hAPI_Labs && cd hAPI_Labs
 
 ### crAPI
 echo "Creating crAPI"
-cd crAPI
+mkdir crAPI && cd crAPI
 curl -o docker-compose.yml https://raw.githubusercontent.com/OWASP/crAPI/main/deploy/docker/docker-compose.yml
 sudo docker-compose pull
 sudo docker-compose -f docker-compose.yml --compatibility up -d
 echo 'Port 8888 is for the main web app and port 8025 is for mailhog'
-cd ..
+cd ~/hAPI_Labs
 
 ### vAPI
 echo "Creating vAPI"
-cd vAPI
 git clone https://github.com/roottusk/vapi.git && cd vapi
 sudo docker-compose up -d
-echo 'Web at is at port 80'
-echo 'Use: docker ps - to view running containers'
+echo 'Web app is at port 80'
+echo 'Use: "docker ps" to view running containers'
 echo 'hAPI Hacking!!!'
